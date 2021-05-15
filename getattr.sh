89e8efea7e28f9f3674b8a81bcb5e67b0c4513c4
Dmitrij <dmitry.minginovich@intexsoft.by>
2021-05-15 16:55:19 +0300

    fixed Find problem

--- getattr.sh
+++ getattr.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-find / | xargs stat -c 'chmod %a %n' > /home/administrator/Documents/$(uname -n)_SysAttr_$(date +"%d-%m-%Y").sh
+find / -print0 | xargs -0 stat -c 'chmod %a %n' > /home/administrator/Documents/$(uname -n)_SysAttr_$(date +"%d-%m-%Y").sh
