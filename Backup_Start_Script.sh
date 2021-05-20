43b2c5467682c35b5f65a240320e653e91d11370
Dmitrij <dmitry.minginovich@intexsoft.by>
2021-05-20 14:21:00 +0300

    corrections

--- Backup_Start_Script.sh
+++ Backup_Start_Script.sh
@@ -20,7 +20,7 @@ fi
 
 for ((i=1; i < 2; i++))
 do 
-    for ((j=1; j <= 5; j++))
+    for ((j=1; j <= 255; j++))
     do
 	expect getattr_on_remote_PC.exp $netw $i $j
 	expect backup_from_pc.exp $netw $i $j
